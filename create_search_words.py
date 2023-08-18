e45f05952fada23a49ed8637e3ce713504567656
IrohasDarknesss <kaine0401mario@gmail.com>
2023-08-19 01:27:59 +0900

    create new code

--- create_search_words.py
+++ create_search_words.py
@@ -4,3 +4,11 @@ with open('search_list/season_query.txt', 'w') as file:
         for season in seasons:
             url = f'/season/{year}-{season}/'
             file.write(url + '\n')
+
+
+with open('search_list/yymm.txt', 'w') as file:
+    for year in range(2015, 2024):
+        seasons = ['Jan', 'Apr', 'Jul', 'Oct']
+        for season in seasons:
+            text = f'{year}_{season}'
+            file.write(text + '\n')
