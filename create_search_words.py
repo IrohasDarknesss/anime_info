da471160847e1957c38fad52b32f8c507934fd8c
IrohasDarknesss <kaine0401mario@gmail.com>
2023-08-19 04:28:37 +0900

    Search Word

--- create_search_words.py
+++ create_search_words.py
@@ -1,14 +1,16 @@
 with open('search_list/season_query.txt', 'w') as file:
-    for year in range(2015, 2024):
-        seasons = ['winter', 'spring', 'summer', 'autumn']
+    for year in range(2021, 2024):
+        #'winter', 'spring'
+        seasons = ['1', '2']
         for season in seasons:
-            url = f'/season/{year}-{season}/'
+            url = f'/year/{year}-{season}.php'
             file.write(url + '\n')
 
 
 with open('search_list/yymm.txt', 'w') as file:
-    for year in range(2015, 2024):
-        seasons = ['Jan', 'Apr', 'Jul', 'Oct']
+    for year in range(2021, 2024):
+        #'Apr', 'Jul'
+        seasons = ['Jul', 'Oct']
         for season in seasons:
             text = f'{year}_{season}'
             file.write(text + '\n')
