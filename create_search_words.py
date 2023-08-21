47c104a4992a9c7e8263b6df1e1384806b2ccc7e
IrohasDarknesss <kaine0401mario@gmail.com>
2023-08-21 09:46:47 +0900

    create 2010-2022 files

--- create_search_words.py
+++ create_search_words.py
@@ -1,16 +1,16 @@
 with open('search_list/season_query.txt', 'w') as file:
-    for year in range(2021, 2024):
+    for year in range(2010, 2023):
         #'winter', 'spring'
-        seasons = ['1', '2']
+        seasons = ['0', '1', '2', '3']
         for season in seasons:
             url = f'/year/{year}-{season}.php'
             file.write(url + '\n')
 
 
 with open('search_list/yymm.txt', 'w') as file:
-    for year in range(2021, 2024):
+    for year in range(2010, 2024):
         #'Apr', 'Jul'
-        seasons = ['Jul', 'Oct']
+        seasons = ['Apr', 'Jul', 'Oct', 'Jan']
         for season in seasons:
             text = f'{year}_{season}'
             file.write(text + '\n')
